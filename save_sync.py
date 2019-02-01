2b9b0bb797dd24c075714ba111257d3c5edc1ed6
watanabe <kouki.1101@outlook.jp>
2019-02-01 13:52:02 +0900

    savefileの名前を変数にした(汎用性のため)

--- save_sync.py
+++ save_sync.py
@@ -5,11 +5,17 @@ import dropbox
 
 dbx = dropbox.Dropbox('1VBymHjFksAAAAAAAAARVCi7nHzqp42BATYy8TdB2cdNtuGqRrJjCujwApWFTyE1')
 
+savefile_on_dropbox = '/todo/todo.save'
+savefile_on_device = './todo.save'
 def save():
-    with open('./todo.save', 'rb') as f:
-        dbx.files_upload(f.read(), '/todo/todo.save',mode=dropbox.files.WriteMode.overwrite)
+    with open(savefile_on_device, 'rb') as f:
+        dbx.files_upload(f.read(), savefile_on_dropbox,mode=dropbox.files.WriteMode.overwrite)
 
 def load():
-    with open('./todo.save', 'wb') as f:
-        metadata, res = dbx.files_download('/todo/todo.save')
+    with open(savefile_on_device, 'wb') as f:
+        metadata, res = dbx.files_download(savefile_on_dropbox)
         f.write(res.content)
+
+if __name__ == '__main__':
+    load()
+
